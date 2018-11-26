77a6a070adb568b9f4243802cd3cd0cfe199243e
Francois284Modz <francoistranchot@gmail.com>
2018-11-26 21:08:59 +0100

    Update GetGeoLocation.cs

--- GetGeoLocation.cs
+++ GetGeoLocation.cs
@@ -15,20 +15,24 @@ namespace Free_GEOIP_By_Francois284Modz
 
         public class IPData
         {
-            public string status { get; set; }
-            public string query { get; set; }
-            public string country { get; set; }
-            public string countryCode { get; set; }
-            public string region { get; set; }
-            public string regionName { get; set; }
-            public string city { get; set; }
-            public string zip { get; set; }
-            public string lat { get; set; }
-            public string lon { get; set; }
-            public string timezone { get; set; }
-            public string isp { get; set; }
-            public string org { get; set; }
-            public string name { get; set; }
+            
+            public string ip {get;set;}
+            public string city {get;set;}
+            public string region {get;set;}
+            public string  country {get;set;}
+            public string loc {get;set;}
+            public string postal {get;set;}
+            
+            /* asn */
+            public string asn {get;set;}
+            public string name {get;set;}
+            public string domain {get;set;}
+            public string route {get;set;}
+            public string type {get;set;}
+            public string isp {get;set;}
+            public string mcc {get;set;}
+            public string mnc {get;set;}
+
  
 
         }
